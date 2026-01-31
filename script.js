3a18b5800ed2f2f70df98bb22f09885b21fa7dfc
PaperRig <aykaytraps@icloud.com>
2026-01-31 22:45:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
+// Flag to track if the API is downloaded or not
+let isApiDownloaded = false;
+
+function downloadApi() {
+  // Start downloading the API
+  window.location.href = 'https://github.com/PaperRig/PaperRigOfficial/raw/main/downloads/paperrig-api.jar';
+  
+  // After downloading, mark the API as downloaded
+  isApiDownloaded = true;
+
+  // Close the modal
+  closeModal();
+  
+  // Optionally, show a message like "API downloaded, now download mod versions."
+  alert("API downloaded! Now you can download v1 or v2.");
+}
+
 function downloadV1() {
-  window.location.href = 'https://github.com/PaperRig/PaperRigOfficial/raw/main/downloads/paperrig-v1.jar';
+  if (!isApiDownloaded) {
+    // If the API isn't downloaded, show the modal
+    openModal();
+  } else {
+    // If API is downloaded, download v1
+    window.location.href = 'https://github.com/PaperRig/PaperRigOfficial/raw/main/downloads/paperrig-v1.jar';
+  }
 }
 
 function downloadV2() {
-  window.location.href = 'https://github.com/PaperRig/PaperRigOfficial/raw/main/downloads/paperrig-v2.jar';
+  if (!isApiDownloaded) {
+    // If the API isn't downloaded, show the modal
+    openModal();
+  } else {
+    // If API is downloaded, download v2
+    window.location.href = 'https://github.com/PaperRig/PaperRigOfficial/raw/main/downloads/paperrig-v2.jar';
+  }
 }
 
-function downloadApi() {
-  window.location.href = 'https://github.com/PaperRig/PaperRigOfficial/raw/main/downloads/paperrig-api.jar';
-  closeModal(); // Close the modal once the API is downloaded
+function openModal() {
+  document.getElementById('modal').style.display = 'block';
 }
 
 function closeModal() {
