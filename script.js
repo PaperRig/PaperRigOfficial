e61ef2014f9c9cf922bf38a3a893b8d290b0e0dd
PaperRig <aykaytraps@icloud.com>
2026-01-31 21:57:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,19 @@
-const API_KEY = "paperrig_api_downloaded";
-
-function isApiDownloaded() {
-  return localStorage.getItem(API_KEY) === "true";
-}
-
-function setApiDownloaded() {
-  localStorage.setItem(API_KEY, "true");
-}
-
-function openModal() {
-  document.getElementById("modal").classList.add("show");
-}
-
-function closeModal() {
-  document.getElementById("modal").classList.remove("show");
-}
-
 function downloadApi() {
-  setApiDownloaded();
-  window.location.href = "downloads/paperrig-api.jar";
-}
-
-function requireApi() {
-  if (!isApiDownloaded()) {
-    openModal();
-    return false;
-  }
-  return true;
+  window.location.href = "downloads/api.zip";
 }
 
 function downloadV1() {
-  if (!requireApi()) return;
-  window.location.href = "downloads/paperrig-v1.jar";
+  window.location.href = "downloads/paperrig-v1.zip";
 }
 
 function downloadV2() {
-  if (!requireApi()) return;
-  window.location.href = "downloads/paperrig-v2.jar";
+  window.location.href = "downloads/paperrig-v2.zip";
+}
+
+function openModal() {
+  document.getElementById("modal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
 }
